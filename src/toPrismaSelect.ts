42cad02eec2e26efa5dba83aee52813906689be5
susantlalshrestha <susantlalshrestha@gmail.com>
2021-09-18 14:08:51 +0545

    added: prisma select return type

--- src/toPrismaSelect.ts
+++ src/toPrismaSelect.ts
@@ -1,13 +1,13 @@
-const toPrismaSelect = <T>(
-  mappedSelection: Record<string, boolean | any>
-): Record<"select", T> => {
+export type PrismaSelect<T> = Record<"select", T>;
+
+const toPrismaSelect = <T>(mappedSelection: Record<string, boolean | any>) => {
   if (!mappedSelection) return;
   const prismaSelect = mappedSelection;
   Object.keys(mappedSelection).forEach((s) => {
     if (typeof prismaSelect[s] === "object")
       prismaSelect[s] = toPrismaSelect(prismaSelect[s]);
   });
-  return { select: prismaSelect as T };
+  return { select: prismaSelect } as PrismaSelect<T>;
 };
 
 export default toPrismaSelect;
