7c3114d3a9aba80ea980df3af72e717ed9257047
susantlalshrestha <susantlalshrestha@gmail.com>
2021-09-22 18:26:31 +0545

    added: default value in toPrismaSelect

--- src/toPrismaSelect.ts
+++ src/toPrismaSelect.ts
@@ -1,7 +1,10 @@
 export type PrismaSelect<T> = Record<"select", T>;
 
-const toPrismaSelect = <T>(mappedSelection: Record<string, boolean | any>) => {
-  if (!mappedSelection) return;
+const toPrismaSelect = <T>(
+  mappedSelection: Record<string, boolean | any>,
+  defaultSelect: any = false
+): PrismaSelect<T> => {
+  if (!mappedSelection) return defaultSelect;
   const prismaSelect = mappedSelection;
   Object.keys(mappedSelection).forEach((s) => {
     if (typeof prismaSelect[s] === "object")
